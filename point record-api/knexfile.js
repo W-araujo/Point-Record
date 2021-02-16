984c9095be3847a5034a4ab68d4046c9bae414b9
Wesley Araujo <wesleyfaraujjo@gmail.com>
2021-02-16 17:50:41 -0300

    added connection to the database and migrations

---
+++ point record-api/knexfile.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const { DB_CLIENT, DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env
+
+module.exports = {
+
+  development: {
+    client: DB_CLIENT,
+    connection: {
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASS,
+      database: DB_NAME,
+      charset: 'utf8'
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: 'src/database/migrations'
+    }
+  },
+};
---
+++ point record-api/src/database/migrations/01_user.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('user', (table) => {
+        table.increments('id')
+        table.string('name', 100).notNullable()
+        table.string('email', 150).notNullable()
+        table.string('password', 150).notNullable()
+        table.string('role', 3).notNullable()
+        table.dateTime('created_at').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP'))
+        table.dateTime('updated_at').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+        table.boolean('is_deleted').default(false)
+      })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('user')
+};
