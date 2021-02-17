dc4034b8be25ac25745d34f6ba5331bcf458be9d
Wesley Araujo <wesleyfaraujjo@gmail.com>
2021-02-17 17:55:08 -0300

    improvements

--- point record-api/src/services/UserService.js
+++ point record-api/src/services/UserService.js
@@ -1,9 +1,10 @@
 const User = require('../models/User')
+const md5 = require("md5")
 
 class UserService {
 
     async create(data) {
-        return User.create(data)
+        return User.create({ name: data.name, email: data.email, password: md5(data.password), role: data.role })
     }
 
 }
