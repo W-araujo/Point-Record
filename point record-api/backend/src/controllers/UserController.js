f75481699802eb8facc2d50bd2c334ef6b61d91d
Wesley Araujo <wesleyfaraujjo@gmail.com>
2021-02-22 13:48:29 -0300

    added img and fix status code

--- point record-api/backend/src/controllers/UserController.js
+++ point record-api/backend/src/controllers/UserController.js
@@ -4,7 +4,7 @@ class UserController {
     async create(req, res) {
         try {
             await UserService.create(req.body)
-            return res.status(200).json({ message: "Registered successfully" })
+            return res.status(201).json({ message: "Registered successfully" })
         } catch (error) {
             console.log(error)
             return res.status(400).json({ message: "Something went wrong, try again" })
